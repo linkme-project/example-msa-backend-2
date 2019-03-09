efd75a30cad9f8f5d0c0c13d3c98a83d24c3c0da
Keunhak Lim <limkeunhak@gmail.com>
2019-03-09 23:47:56 +0900

    commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.status(200).json([{ key:1, value:"log 1" }, { key: 2, value:"log 2"}, { key: 3, value: "log 3"}]);
 });
 
 module.exports = router;
